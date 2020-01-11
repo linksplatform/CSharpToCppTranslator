12d8e0ba77af68601d980db70501fc7a412b112d
Константин Дьяченко <konard@me.com>
2020-01-11 21:49:17 +0300

    Code cutouts adjusted.

--- CustomCSharpToCppTransformer.cs
+++ CustomCSharpToCppTransformer.cs
@@ -11,11 +11,11 @@ namespace CSharpToCppTranslator
         public static readonly IList<ISubstitutionRule> Rules = new List<SubstitutionRule>
         {
             // Just delete it in GenericCollectionMethodsBase.cs
-            (new Regex(@"virtual TElement GetZero(.|\s)+Increment\(One\)(.|\s)+?}"), "", new Regex(@"GenericCollectionMethodsBase\.cs"), 0),
+            (new Regex(@"\r?\n[\t ]+[^\r\n]*GetZero(.|\s)+Increment\(One\)(.|\s)+?}"), "", new Regex(@"GenericCollectionMethodsBase\.cs"), 0),
             // Just delete it in SizedBinaryTreeMethodsBase.cs
-            (new Regex(@"\r?\n[\t ]+void FixSizes(.|\s)+};"), "    };", new Regex(@"SizedBinaryTreeMethodsBase\.cs"), 0),
+            (new Regex(@"\r?\n[\t ]+[^\r\n]*FixSizes(.|\s)+};"), "    };", new Regex(@"SizedBinaryTreeMethodsBase\.cs"), 0),
             // Just delete it in SizedAndThreadedAVLBalancedTreeMethods.cs
-            (new Regex(@"\r?\n[\t ]+void PrintNode(.|\s)+?}[\t ]*\r?\n"), "", new Regex(@"SizedAndThreadedAVLBalancedTreeMethods\.cs"), 0),
+            (new Regex(@"\r?\n[\t ]+[^\r\n]*PrintNode(.|\s)+?}[\t ]*\r?\n"), "", new Regex(@"SizedAndThreadedAVLBalancedTreeMethods\.cs"), 0),
             // TElement path[MaxPath] = { {0} }; 
             // TElement path[MaxPath]; path[0] = 0;
             (new Regex(@"TElement path\[([_a-zA-Z0-9]+)\] = \{ \{0\} \};"), "TElement path[$1]; path[0] = 0;", new Regex(@"SizedAndThreadedAVLBalancedTreeMethods\.cs"), 0),
