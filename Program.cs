00239c18a16679f53da70c45da50086847ebbc6b
Konstantin Diachenko <drakonard@gmail.com>
2020-02-28 13:55:49 +0300

    Build fix.

--- Program.cs
+++ Program.cs
@@ -6,7 +6,10 @@ namespace CSharpToCppTranslator
 {
     public class Program
     {
-        static int Main(string[] args)
+        private const string SourceFileExtension = ".cs";
+        private const string TargetFileExtension = ".cpp";
+
+        static void Main(string[] args)
         {
             //for (int i = 0; i < args.Length; i++)
             //{
@@ -14,14 +17,8 @@ namespace CSharpToCppTranslator
             //}
             //args = new string[] { @"D:\CodeArchive\Links\Ranges\csharp\Platform.Ranges\EnsureExtensions.cs", @"D:\CodeArchive\Links\Ranges\cpp\Platform.Ranges\EnsureExtensions.cpp", "debug" };
             var csharpToCpp = new CustomCSharpToCppTransformer();
-            var transformer = IsDebugModeRequested(args) ? new DebugTransformerDecorator(csharpToCpp, ".cpp") : (ITransformer)new CustomCSharpToCppTransformer();
-            var cli = new TransformerCLI(transformer);
-            var success = cli.Run(args, out string message);
-            if (!string.IsNullOrWhiteSpace(message))
-            {
-                Console.WriteLine(message);
-            }
-            return success ? 0 : -1;
+            var transformer = IsDebugModeRequested(args) ? new LoggingFileTransformer(csharpToCpp, SourceFileExtension, TargetFileExtension) : new FileTransformer(csharpToCpp, SourceFileExtension, TargetFileExtension);
+            new TransformerCLI(transformer).Run(args);
         }
 
         static private bool IsDebugModeRequested(string[] args) => args.TryGetElement(2, out string thirdArgument) ? string.Equals(thirdArgument, "debug", StringComparison.OrdinalIgnoreCase) : false;
