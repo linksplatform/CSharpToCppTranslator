c0c8292e6991943d76f8907a7f26a82661edb7c4
FreePhoenix <freephoenix888@gmail.com>
2021-10-03 18:56:14 +0600

    Update XML Doc

--- Program.cs
+++ Program.cs
@@ -15,31 +15,9 @@ namespace CSharpToCppTranslator
     /// </summary>
     public static class Program
     {
-        /// <summary>
-        /// <para>
-        /// The default source file extension.
-        /// </para>
-        /// <para></para>
-        /// </summary>
         private const string DefaultSourceFileExtension = ".cs";
-        /// <summary>
-        /// <para>
-        /// The default target file extension.
-        /// </para>
-        /// <para></para>
-        /// </summary>
         private const string DefaultTargetFileExtension = ".h";
 
-        /// <summary>
-        /// <para>
-        /// Main the args.
-        /// </para>
-        /// <para></para>
-        /// </summary>
-        /// <param name="args">
-        /// <para>The args.</para>
-        /// <para></para>
-        /// </param>
         private static void Main(string[] args)
         {
             if (args.IsNullOrEmpty())
@@ -60,52 +38,10 @@ namespace CSharpToCppTranslator
             new TransformerCLI(transformer).Run(args);
         }
 
-        /// <summary>
-        /// <para>
-        /// Gets the source file extension using the specified args.
-        /// </para>
-        /// <para></para>
-        /// </summary>
-        /// <param name="args">
-        /// <para>The args.</para>
-        /// <para></para>
-        /// </param>
-        /// <returns>
-        /// <para>The string</para>
-        /// <para></para>
-        /// </returns>
         private static string GetSourceFileExtension(string[] args) => args.TryGetElement(2, out var sourceFileExtension) ? sourceFileExtension : DefaultSourceFileExtension;
 
-        /// <summary>
-        /// <para>
-        /// Gets the target file extension using the specified args.
-        /// </para>
-        /// <para></para>
-        /// </summary>
-        /// <param name="args">
-        /// <para>The args.</para>
-        /// <para></para>
-        /// </param>
-        /// <returns>
-        /// <para>The string</para>
-        /// <para></para>
-        /// </returns>
         private static string GetTargetFileExtension(string[] args) => args.TryGetElement(3, out var targetFileExtension) ? targetFileExtension : DefaultTargetFileExtension;
 
-        /// <summary>
-        /// <para>
-        /// Determines whether is debug mode requested.
-        /// </para>
-        /// <para></para>
-        /// </summary>
-        /// <param name="args">
-        /// <para>The args.</para>
-        /// <para></para>
-        /// </param>
-        /// <returns>
-        /// <para>The bool</para>
-        /// <para></para>
-        /// </returns>
         private static bool IsDebugModeRequested(string[] args) => args.TryGetElement(4, out var debugArgument) && string.Equals(debugArgument, "debug", StringComparison.OrdinalIgnoreCase);
     }
 }
